3eb29152eb734e1c7f0020f9202a2a9223cce63a
Brayan Mejia Cuenca <mejiabrayan@protonmail.com>
2024-07-29 17:07:44 -0700

    intial commit

---
+++ db/seed.ts
@@ -0,0 +1,15 @@
+import { db, Comment } from 'astro:db';
+
+// https://astro.build/db/seed
+export default async function seed() {
+  await db.insert(Comment).values([
+    {
+      author: 'Cody',
+      body: 'This is a comment',
+    },
+    {
+      author: 'Brayan',
+      body: 'This is another comment',
+    },
+  ]);
+}
